0cd535e22552f6d2460cbd028b4a0427aa5e1234
Oussama Ezzayer <oussama@ulule.com>
2025-12-01 08:49:16 +0000

    ref(alert)): adapt to new in-house API

--- src/alert/index.js
+++ src/alert/index.js
@@ -1,3 +1,7 @@
+CURRENT_LOCALE = 'fr'
+ENABLE_PROJECT_NAME = true
+ENABLE_FREE_TIER = false
+
 window.addEventListener('onWidgetLoad', async (obj) => {
   const apiToken = obj.detail.channel.apiToken
 
@@ -24,102 +28,117 @@ window.addEventListener('onWidgetLoad', async (obj) => {
     return logo.outerHTML
   }
 
-  socket.on('contribution', (data) => {
+  socket.on('orders', (data) => {
     const cardElement = document.createElement('div')
-    const userName = Boolean(data.userName) ? data.userName : 'Une personne anonyme'
-    const tip = Number(data.tip)
 
-    if (!tip) {
-      // Cas de contribution sans don libre
+    const { currency, user, rewards } = data
+    const userName = user.user_name ?? 'Une personne anonyme'
+
+    const tip = Number(data.tip)
+    const tipLabel = tip && tip > 0 ? ` + un don de <span id="tip">${tip} ${currency}</span>` : ''
+
+    // Order with a tip only (no reward)
+    if (rewards === null || rewards.length === 0) {
       cardElement.innerHTML = `
         <div class="card slideDown">
           <div class="logo"></div>
-          <p><span id="username">${userName}</span> vient de choisir la contrepartie <span id="subname">« ${data.rewardName} »</span> !</p>
+          <p>Merci <span id="username">${userName}</span> pour le don de <span id="tip">${tip} ${currency}</span> !</p>
+        </div>`
+    } else if (rewards.length === 1) {
+      // Order with a single reward
+      const reward = rewards[0]
+      cardElement.innerHTML = `
+        <div class="card slideDown">
+          <div class="logo"></div>
+          <p><span id="username">${userName}</span> vient de choisir la contrepartie <span id="subname">« ${getI18n(reward.title)} »</span> ${tipLabel} !</p>
         </div>`
     } else {
-      // Cas de contribution avec don libre
+      // Order with multiple rewards
+      const titles = rewards.map((reward) => {
+        return `<span id="subname">« ${getI18n(reward.title)} »</span>`
+      })
       cardElement.innerHTML = `
         <div class="card slideDown">
           <div class="logo"></div>
-          <p><span id="username">${userName}</span> vient de choisir <span id="subname">« ${data.rewardName} »</span> + un don de <span id="tip">${tip} ${data.currency}</span> !</p>
+          <p><span id="username">${userName}</span> vient de choisir les contreparties ${titles.join(', ')} ${tipLabel} !</p>
         </div>`
     }
 
     showElement(cardElement)
   })
 
-  socket.on('sub', (data) => {
+  socket.on('subscriptions', (data) => {
     const cardElement = document.createElement('div')
 
-    // Si il y a un don
-    if (data.tip) {
-      // Cas d'utilisateur·ice déja abonné·e
-      if (data.subName) {
-        if (data.years !== 0 && data.years !== 'null' && data.years !== '0' && data.years !== undefined) {
+    const { currency, is_recurring: isRecurring, months, project, reward, total, tip, user, years } = data
+    const userName = user.user_name ?? 'Une personne anonyme'
+    const rewardTitle = getI18n(reward.title)
+
+    const projectLabel = ENABLE_PROJECT_NAME ? `à <span id="project">${getI18n(project.title)}</span>` : ''
+    const tipLabel = `<p>Merci <span id="username">${userName}</span> pour le don de <span id="tip">${tip} ${currency}</span> ${projectLabel} !`
+
+
+    // Recurring donation to a membership-based project
+    if (isRecurring && total > 0) {
+      cardElement.innerHTML = `
+        <div class="card slideDown">
+          <div class="logo"></div>
+          <p>Merci <span id="username">${userName}</span> pour le don mensuel de <span id="tip">${total} ${currency}</span> ${projectLabel} !</p>
+        </div>`
+    }
+    // Membership with a tip
+    else if (tip) {
+      // Running membership
+      if (rewardTitle) {
+        // Membership older than a year
+        if (years !== 0 && years !== 'null' && years !== '0' && years !== undefined) {
           cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour le don de <span id="tip">${data.tip} ${data.currency}</span> ! Abonné·e au niveau <span id="subname">"${data.subName}"</span> depuis ${data.years} an(s) et ${data.months} mois</p>
+             ${tipLabel} Abonné·e au niveau <span id="subname">"${rewardTitle}"</span> depuis ${years} an(s) et ${months} mois</p>
           </div>`
-        } else if (data.months !== 0 && data.months !== 'null' && data.months !== '0' && data.months !== undefined) {
+        // Membership of less than a year
+        } else if (months !== 0 && months !== 'null' && months !== '0' && months !== undefined) {
           cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour le don de <span id="tip">${data.tip} ${data.currency}</span> ! Abonné·e au niveau <span id="subname">"${data.subName}"</span> depuis ${data.months} mois</p>
+            ${tipLabel} Abonné·e au niveau <span id="subname">"${rewardTitle}"</span> depuis ${months} mois</p>
           </div>`
+        // New membership
         } else {
           cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour le don de <span id="tip">${data.tip} ${data.currency}</span> ! Abonné·e au niveau <span id="subname">"${data.subName}"</span></p>
+            ${tipLabel} Abonné·e au niveau <span id="subname">"${rewardTitle}"</span></p>
           </div>`
-        }
-      } else {
-        // Si c'est un don récurrent
-        if (data.isRecurringDonation === 'True' && data.recurringDonationAmount > 0) {
-          cardElement.innerHTML = `
-            <div class="card slideDown">
-              <div class="logo"></div>
-              <p>Merci <span id="username">${data.userName}</span> pour le don mensuel de <span id="tip">${data.recurringDonationAmount} ${data.currency}</span> !</p>
-            </div>`
-        } else {
-          // Si c'est un don unique
-          cardElement.innerHTML = `
-              <div class="card slideDown">
-                <div class="logo"></div>
-                <p>Merci <span id="username">${data.userName}</span> pour le don de <span id="tip">${data.tip} ${data.currency}</span> !</p>
-              </div>`
         }
       }
     } else {
-      // Cas d'utilisateur·ice déja abonné·e, sans don
-      if (data.years !== 0 && data.years !== 'null' && data.years !== '0' && data.years !== undefined) {
+      // Running membership, no tip
+      if (years !== 0 && years !== 'null' && years !== '0' && years !== undefined) {
         cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour les ${data.years} an(s) et ${data.months} mois d'abonnement au niveau <span id="subname">"${data.subName}"</span> !</p>
+            <p>Merci <span id="username">${userName}</span> pour les ${years} an(s) et ${months} mois d'abonnement au niveau <span id="subname">"${rewardTitle}"</span> !</p>
           </div>`
-      } else if (data.months !== 0 && data.months !== 'null' && data.months !== '0' && data.months !== undefined) {
+      } else if (months !== 0 && months !== 'null' && months !== '0' && months !== undefined) {
         cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour les ${data.months} mois d'abonnement au niveau <span id="subname">"${data.subName}"</span> !</p>
+            <p>Merci <span id="username">${userName}</span> pour les ${months} mois d'abonnement au niveau <span id="subname">"${rewardTitle}"</span> !</p>
           </div>`
-      } else if (data.subName) {
+      } else if (reward) {
         cardElement.innerHTML = `
           <div class="card slideDown">
             <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour le nouvel abonnement au niveau <span id="subname">"${data.subName}"</span> !</p>
-          </div>`        
-      } else {
-        // We're disabling free tier notifications for now. Shall be configurable later.
-        /**
-        cardElement.innerHTML = `
-          <div class="card slideDown">
-            <div class="logo"></div>
-            <p>Merci <span id="username">${data.userName}</span> pour le nouvel abonnement gratuit !</p>
+            <p>Merci <span id="username">${userName}</span> pour le nouvel abonnement ${projectLabel} au niveau <span id="subname">"${rewardTitle}"</span> !</p>
           </div>`
-        */
+      } else if (ENABLE_FREE_TIER) {
+        cardElement.innerHTML = `
+          <div class="card slideDown">
+            <div class="logo"></div>
+            <p>Merci <span id="username">${userName}</span> pour le nouvel abonnement gratuit !</p>
+          </div>`
       }
     }
 
@@ -139,5 +158,33 @@ window.addEventListener('onWidgetLoad', async (obj) => {
 
   function onAuthenticated(data) {
     console.log(`[ulule-widget] Successfully connected to channel ${data.channelId}`)
+  }
+
+  // IN-HOUSE HELPERS
+
+  /**
+   * Retrieves the best-matched localized string from an i18n resource object.
+   *
+   * The function attempts to return:
+   * 1. The value for the current locale (via `CURRENT_LOCALE`), if present.
+   * 2. Otherwise, the English (`"en"`) value, if present.
+   * 3. Otherwise, the first available value in the resource object.
+   *
+   * @param {Object.<string, string>} resource
+   *   An object mapping locale codes (e.g., `"en"`, `"fr"`, `"es"`) to localized strings.
+   *
+   * @returns {string}
+   *   The localized string that best matches the current locale fallback rules.
+   */
+  function getI18n(resource) {
+    if (resource[CURRENT_LOCALE]) {
+      return resource[CURRENT_LOCALE]
+    }
+
+    if (resource['en']) {
+      return resource['en']
+    }
+
+    return Object.values(resource)[0]
   }
 })
